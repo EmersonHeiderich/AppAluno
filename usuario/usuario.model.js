57b580f3b70c9b80fc97df5983ccfe4b0db547ad
Emerson <emerson.sas@gmail.com>
2018-10-29 07:56:19 -0300

    Adicionado Curso e Semestre no Schema de login

--- usuario/usuario.model.js
+++ usuario/usuario.model.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema;
 var schemaUsuario = new Schema ({
     login : String,
     senha : String,
-    nome : String
+    nome : String,
+    curso : {
+        type : Types.ObjectId,
+        ref : 'Curso'
+    },
+    semestre : {
+        type : Types.ObjectId,
+        ref : 'Semestre'
+    }
+
 });
 
 module.exports = mongoose.model("Usuario", schemaUsuario);
